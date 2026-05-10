d8bb54c57618d9f3bbd0173a18c5216db05d0f17
visheshtechie <visheshtechie@gmail.com>
2026-05-10 17:07:01 +0000

    [cicd] Committing 2 items from workspace 97ca4401-7b9f-4ffe-bde5-70288bc64837

--- cicd/CD004_NB.Notebook/notebook-content.py
+++ cicd/CD004_NB.Notebook/notebook-content.py
@@ -8,31 +8,16 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse": "32a5b01d-d56f-4400-95ac-37198d7ef67c",
-# META       "default_lakehouse_name": "CD004_LH2",
+# META       "default_lakehouse": "2373f2ef-99ee-4715-b20b-5cf0856f1eac",
+# META       "default_lakehouse_name": "CD004_LH",
 # META       "default_lakehouse_workspace_id": "97ca4401-7b9f-4ffe-bde5-70288bc64837",
 # META       "known_lakehouses": [
 # META         {
 # META           "id": "2373f2ef-99ee-4715-b20b-5cf0856f1eac"
-# META         },
-# META         {
-# META           "id": "32a5b01d-d56f-4400-95ac-37198d7ef67c"
 # META         }
 # META       ]
-# META     },
-# META     "environment": {}
+# META     }
 # META   }
-# META }
-
-# PARAMETERS CELL ********************
-
-my_parameter = 1
-
-# METADATA ********************
-
-# META {
-# META   "language": "python",
-# META   "language_group": "synapse_pyspark"
 # META }
 
 # CELL ********************
