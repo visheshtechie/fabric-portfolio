4d658997fb47e067b72a264a6681aa24be0511ba
visheshtechie <visheshtechie@gmail.com>
2026-05-10 12:14:33 +0000

    [cicd] Committing 2 items from workspace 97ca4401-7b9f-4ffe-bde5-70288bc64837

--- cicd/CD004_NB.Notebook/notebook-content.py
+++ cicd/CD004_NB.Notebook/notebook-content.py
@@ -6,7 +6,18 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "2373f2ef-99ee-4715-b20b-5cf0856f1eac",
+# META       "default_lakehouse_name": "CD004_LH",
+# META       "default_lakehouse_workspace_id": "97ca4401-7b9f-4ffe-bde5-70288bc64837",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "2373f2ef-99ee-4715-b20b-5cf0856f1eac"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # CELL ********************
@@ -17,5 +28,17 @@ print('hello world')
 
 # META {
 # META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+# MAGIC %%sparkr
+# MAGIC print("Hello World")
+
+# METADATA ********************
+
+# META {
+# META   "language": "r",
 # META   "language_group": "synapse_pyspark"
 # META }
